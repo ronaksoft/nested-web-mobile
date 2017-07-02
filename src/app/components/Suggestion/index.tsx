74dd195b42235047f021581efbf56b3e84ff77ab
Ali <me@robzizo.ir>
2017-07-02 10:23:49 +0430

    early start on suggostion

---
+++ src/app/components/Suggestion/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Input } from 'antd';
+import 'antd/dist/antd.css';
+
+class Suggestion extends React.Component<any, any> {
+  public render() {
+    return (
+      <div>
+        <Input></Input>
+      </div>
+    );
+  }
+}
+
+export { Suggestion }
--- src/app/scenes/private/compose/index.tsx
+++ src/app/scenes/private/compose/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
+import { Suggestion } from 'components';
 
 class Compose extends React.Component<any, any> {
   public render() {
     return (
       <div>
-        Compose
+        <Suggestion></Suggestion>
       </div>
     );
   }
