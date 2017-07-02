2f329160271f4bb9fb8d476c9ef7f6efdc0edff2
Sina <e.hosseini.ir@gmail.com>
2017-07-02 11:53:26 +0430

    add antd to project

--- src/app/scenes/private/posts/index.tsx
+++ src/app/scenes/private/posts/index.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
+import {Input} from 'antd';
+
+import 'antd/dist/antd.css';
 
 class Posts extends React.Component<any, any> {
   public render() {
     return (
       <div>
         Posts
+        <Input/>
       </div>
     );
   }
