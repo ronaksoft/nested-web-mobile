d268c16945c6aad1e1d5a6f8af0d37155e789737
shayesteh <naeimabadi.shayesteh@gmail.com>
2017-07-03 08:52:50 +0430

    import component for rendering

--- src/app/scenes/private/files/index.tsx
+++ src/app/scenes/private/files/index.tsx
@@ -3,6 +3,7 @@ import { FileItem } from 'components/FileItem';
 import 'antd/dist/antd.css';
 
 class Files extends React.Component<any, any> {
+
   public render() {
     return (
       <div>
--- src/app/scenes/public/signin/index.tsx
+++ src/app/scenes/public/signin/index.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
+import { FileItem } from '../../../components/FileItem';
 
 class Signin extends React.Component<any, any> {
   public render() {
     return (
       <div>
         Signin
+        <FileItem/>
       </div>
     );
   }
