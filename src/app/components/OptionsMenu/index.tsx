cdfd1a2a7394b470d33f5e4ecc3496264351fec7
Ali <me@robzizo.ir>
2017-07-06 12:45:20 +0430

    icons and options menu initial commit

--- src/app/components/OptionsMenu/index.tsx
+++ src/app/components/OptionsMenu/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import IPlace from '../../api/place/interfaces/IPlace';
-import {Icon} from 'antd';
 import {IcoN} from 'components';
 const style = require('./OptionsMenu.css');
 
@@ -18,14 +17,18 @@ class OptionsMenu extends React.Component<IOptionsMenuProps, any> {
   public render() {
     return (
     <div className={style.visible}>
-        <a>
-          adwdwd
-          <IcoN size={24} name="arrow24"/>
+        <a className={style.title}>
+          adwdwdds
+          <IcoN size={24} name="arrowSense24"/>
         </a>
         <div className={style.filler}/>
         <div className={style.icons}>
-          <Icon type="link"  />
-          <Icon type="link"  />
+          <div className={style.icon}>
+            <IcoN size={24} name="sort24"/>
+          </div>
+          <div className={style.icon}>
+            <IcoN size={24} name="sort24"/>
+          </div>
         </div>
     </div>
     );
