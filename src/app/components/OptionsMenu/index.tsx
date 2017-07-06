d675cf05bb53c8bf0a97e4dc2a1594b266d43564
Ali <me@robzizo.ir>
2017-07-06 14:29:07 +0430

    Feed and options menu on the go

--- src/app/components/OptionsMenu/index.tsx
+++ src/app/components/OptionsMenu/index.tsx
@@ -7,29 +7,67 @@ interface IOptionsMenuProps {
   item?: IPlace | null;
   filter?: string;
 }
+interface IOptionsMenuStat {
+  titlePopup?: boolean;
+  iconIPopup?: boolean;
+  iconIIPopup?: boolean;
+}
 
-class OptionsMenu extends React.Component<IOptionsMenuProps, any> {
+class OptionsMenu extends React.Component<IOptionsMenuProps, IOptionsMenuStat> {
 
   constructor(props: any) {
     super(props);
   }
 
+  public componentWillMount() {
+    this.setState({
+      titlePopup: false,
+      iconIPopup: false,
+      iconIIPopup: false,
+    });
+  }
+
+  public openPopUp = (wrapper: string) => {
+    const key = wrapper + 'Popup';
+    this.setState({
+      titlePopup: false,
+      iconIPopup: false,
+      iconIIPopup: false,
+    });
+    this.setState({
+      [key]: !this.state[key],
+    });
+  }
+
   public render() {
     return (
-    <div className={style.visible}>
-        <a className={style.title}>
+    <div className={style.container}>
+      <div className={style.visible}>
+        <a onClick={this.openPopUp.bind(this, 'title')}
+        className={this.state.titlePopup ? style.title + ' ' + style.active : style.title}>
           adwdwdds
           <IcoN size={24} name="arrowSense24"/>
         </a>
         <div className={style.filler}/>
         <div className={style.icons}>
-          <div className={style.icon}>
+          <div className={this.state.iconIPopup ? style.icon + ' ' + style.active : style.icon}
+          onClick={this.openPopUp.bind(this, 'iconI')}>
             <IcoN size={24} name="sort24"/>
           </div>
-          <div className={style.icon}>
+          <div className={this.state.iconIIPopup ? style.icon + ' ' + style.active : style.icon}
+          onClick={this.openPopUp.bind(this, 'iconII')}>
             <IcoN size={24} name="sort24"/>
           </div>
         </div>
+      </div>
+      { (this.state.titlePopup || this.state.iconIPopup || this.state.iconIIPopup) &&
+        <div className={style.invisible}>
+        a
+        </div>
+      }
+      { this.state.titlePopup &&
+        <div className={style.overlay}/>
+      }
     </div>
     );
   }
