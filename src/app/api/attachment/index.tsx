4ed595dc4240eb69b118aaa8b4d3acad1ca114fe
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2017-07-10 18:39:06 +0430

    Add AttachmentApi with upload method

---
+++ src/app/api/attachment/index.tsx
@@ -0,0 +1,102 @@
+import UploadType from './UploadType';
+import IGetUploadTokenResponse from './interfaces/IGetUploadTokenResponse';
+import Api from 'api';
+import Configuration from 'config';
+import AAA from 'services/aaa';
+
+interface IThumbnails {
+  org: string;
+  pre: string;
+  x32: string;
+  x64: string;
+  x128: string;
+}
+
+interface IAttachment {
+  expiration_timestamp: number;
+  name: string;
+  size: string;
+  thumbs: IThumbnails;
+  type: string;
+  universal_id: string;
+}
+
+class AttachmentApi {
+  private static getUploadToken(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      Api.getInstance().request({
+        cmd: 'file/get_upload_token',
+        data: {},
+      }).then((response: IGetUploadTokenResponse) => {
+        resolve(response.token);
+      }, reject);
+    });
+  }
+
+  private static getUploadUrl(storeUrl: string, type: string, sk: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      this.getUploadToken().then((token: string) => {
+        resolve(`${storeUrl}/upload/${type}/${sk}/${token}`);
+      }, reject);
+    });
+  }
+
+  public static upload(file: File, type: string, onProgress: any): Promise<IAttachment> {
+
+    const sessionKey = AAA.getInstance().getCredentials().sk;
+    const storeUrl = Configuration.STORE.URL;
+    const formData = new FormData();
+    const xhr = new XMLHttpRequest();
+
+    return new Promise((resolve, reject) => {
+      formData.append('file', file);
+      this.getUploadUrl(storeUrl, type, sessionKey).then((url) => {
+
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader('Cache-Control', 'no-cache');
+        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        xhr.setRequestHeader('X-File-Size', file.size.toString());
+        xhr.setRequestHeader('X-File-Type', file.type);
+
+        if (onProgress) {
+          xhr.upload.onprogress = (e: any) => {
+            onProgress(e.loaded, e.total);
+          };
+        }
+
+        xhr.upload.onerror = (e: any) => {
+          reject(e);
+        };
+
+        xhr.onload = (e: any) => {
+          if (200 !== e.target.status) {
+            return reject(e);
+          }
+
+          const response = JSON.parse(e.target.response);
+
+          switch (response.data.status) {
+            case 'ok':
+              resolve(response);
+              break;
+
+            default:
+              reject(response);
+              break;
+          }
+        };
+
+        xhr.upload.onabort = (e: any) => {
+          reject(e);
+        };
+
+        xhr.send(formData);
+      }, reject);
+    });
+  }
+}
+
+export default AttachmentApi;
+export {
+  UploadType
+};
