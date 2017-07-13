371a5b66b092addfef2e08962b5c126f250ea02d
shayesteh <naeimabadi.shayesteh@gmail.com>
2017-07-13 15:40:18 +0430

    add multiaccount  notification comment

--- src/app/scenes/private/notifications/NotificationItem/Comment/index.tsx
+++ src/app/scenes/private/notifications/NotificationItem/Comment/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import INotification from '../../../../../api/notification/interfaces/INotification';
 import TimeUntiles from '../../../../../services/untils/time';
 import {IcoN, UserAvatar, FullName} from 'components';
+import IUser from '../../../../../api/account/interfaces/IUser';
 import CommentBody from '../../../../../components/CommentBody';
 import 'antd/dist/antd.css';
 
@@ -9,26 +10,48 @@ const style = require('../NotificationItem.css');
 
 interface IProps {
   notification: INotification;
+  accounts: IUser[];
 }
 
 class Comment extends React.Component <IProps, any> {
   public render() {
-    return (
+    if (this.props.notification.others) {
+      return (
       <div className={style.mention}>
         <div className={style.notifWrapper}>
-            <UserAvatar user_id={this.props.notification.actor_id} size={32} borderRadius={'16px'}/>
+          <UserAvatar user_id={this.props.notification.actor_id} size={32} borderRadius={'16px'}/>
           <div className={style.notifContainer}>
             <div className={style.notifData}>
               <b><FullName user_id={this.props.notification.actor_id}/></b>
-              <span> commented: </span>
-              <CommentBody comment_id={this.props.notification.comment_id} post_id={this.props.notification.post_id}/>.
+              <span> commented on your post: </span>
+              <CommentBody comment_id={this.props.notification.comment_id}
+                           post_id={this.props.notification.post_id}/>.
               <span> {TimeUntiles.dynamic(this.props.notification.timestamp)}</span>
             </div>
             <IcoN size={16} name={'comment24'}/>
           </div>
         </div>
       </div>
-    );
+      );
+    } else {
+      return (
+        <div className={style.mention}>
+          <div className={style.notifWrapper}>
+            <UserAvatar user_id={this.props.notification.actor_id} size={32} borderRadius={'16px'}/>
+            <div className={style.notifContainer}>
+              <div className={style.notifData}>
+                <b><FullName user_id={this.props.notification.actor_id}/></b>
+                <span> commented on your post: </span>
+                <CommentBody comment_id={this.props.notification.comment_id}
+                             post_id={this.props.notification.post_id}/>.
+                <span> {TimeUntiles.dynamic(this.props.notification.timestamp)}</span>
+              </div>
+              <IcoN size={16} name={'comment24'}/>
+            </div>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
