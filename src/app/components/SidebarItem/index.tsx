a2d0c29b5ee18afc76c5e054ebf4fd3ba1c17cbe
Sina <e.hosseini.ir@gmail.com>
2017-07-15 19:32:03 +0430

    close sidebar after click

--- src/app/components/SidebarItem/index.tsx
+++ src/app/components/SidebarItem/index.tsx
@@ -1,4 +1,4 @@
- import * as React from 'react';
+import * as React from 'react';
 import ISidebarPlace from '../../api/place/interfaces/ISidebarPlace';
 import {IcoN, PlaceItem, PlaceName} from 'components';
 import {Link} from 'react-router';
@@ -11,6 +11,7 @@ interface ISidebarItemProps {
   key: string;
   childrenUnread: boolean;
   unreads: number;
+  onClick: (place?: ISidebarPlace) => {};
 }
 
 class SidebarItem extends React.Component<ISidebarItemProps, any> {
@@ -20,48 +21,48 @@ class SidebarItem extends React.Component<ISidebarItemProps, any> {
   }
 
   private onItemClick = (e) => {
-      this.props.openChild();
-      e.preventDefault();
-      return false;
+    this.props.openChild();
+    e.preventDefault();
+    return false;
   }
 
   public render() {
     const placeIndent = [];
     console.log(this.props.unreads, this.props.childrenUnread);
     for (let i: number = 0; i < this.props.place.depth; i++) {
-        placeIndent.push(
-            <div key={this.props.place.id + i + 'b'} className={style.indent}/>,
-        );
+      placeIndent.push(
+        <div key={this.props.place.id + i + 'b'} className={style.indent}/>,
+      );
     }
     return (
       <li key={this.props.key}>
         {!this.props.place.isChildren &&
-            <hr className={style.hrDark}/>
+        <hr className={style.hrDark}/>
         }
         {!this.props.place.isChildren &&
-            <hr className={style.hrLight}/>
+        <hr className={style.hrLight}/>
         }
-        <Link to={`/posts/${this.props.place.id}`} activeClassName="active">
-            <div className={style.place}>
-                {placeIndent}
-                <PlaceItem place_id={this.props.place.id} size={24} borderRadius="3px"/>
-                <div className={style.indent}/>
-                <PlaceName place_id={this.props.place.id}/>
-                {this.props.unreads > 0 &&
-                    <b>{this.props.unreads}</b>
-                }
-                <div className={[style.childArrow, this.props.place.isOpen ? style.active : null].join(' ')}
-                    onClick={this.onItemClick}>
-                    {this.props.place.hasChildren &&
-                        <IcoN size={16} name={'arrow16White'}/>
-                    }
-                    {this.props.childrenUnread &&
-                        <IcoN size={8} name={'circle8blue'}/>
-                    }
-                </div>
+        <Link to={`/posts/${this.props.place.id}`} activeClassName="active" onClick={this.props.onClick.bind(this)}>
+          <div className={style.place}>
+            {placeIndent}
+            <PlaceItem place_id={this.props.place.id} size={24} borderRadius="3px"/>
+            <div className={style.indent}/>
+            <PlaceName place_id={this.props.place.id}/>
+            {this.props.unreads > 0 &&
+            <b>{this.props.unreads}</b>
+            }
+            <div className={[style.childArrow, this.props.place.isOpen ? style.active : null].join(' ')}
+                 onClick={this.onItemClick}>
+              {this.props.place.hasChildren &&
+              <IcoN size={16} name={'arrow16White'}/>
+              }
+              {this.props.childrenUnread &&
+              <IcoN size={8} name={'circle8blue'}/>
+              }
             </div>
+          </div>
         </Link>
-        </li>
+      </li>
     );
   }
 }
