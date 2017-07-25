9c2ae86056b75c066acac3923246f7001a9ce8a9
Ali <me@robzizo.ir>
2017-07-25 14:04:50 +0430

    server service documentation

--- src/app/services/server/index.tsx
+++ src/app/services/server/index.tsx
@@ -1,3 +1,13 @@
+/**
+ * @file services/server/index.tsx
+ * @author -
+ * @desc Generates a unique number starting from 1
+ * @export {Unique}
+ * Documented by: robzizo
+ * Date of documentation:  2017-07-25
+ * Reviewed by:            -
+ * Date of review:         -
+ */
 import IRequest from './interfaces/IRequest';
 import IResponse from './interfaces/IResponse';
 import IErrorResponseData from './interfaces/IErrorResponseData';
@@ -9,15 +19,64 @@ import Unique from './../utils/unique';
 
 const TIMEOUT_TIME = 24000;
 
+/**
+ *
+ * @class Server
+ * @requires [<AAA>,<Client>]
+ */
 class Server {
+
+  /**
+   * Define socket
+   * @private
+   * @type {any}
+   * @memberof Server
+   */
   private socket: any = null;
+
+  /**
+   * @name Server#queue
+   * @private
+   * @type {any}
+   * @memberof Server
+   */
   private queue: any;
+
+  /**
+   * @name Server#reqId
+   * @private
+   * @type {number}
+   * @memberof Server
+   */
   private reqId: number = Date.now();
+
+  /**
+   * @name Server#cid
+   * @private
+   * @type {string}
+   * @memberof Server
+   */
   private cid: string;
+
+  /**
+   * @name Server#messageListeners
+   * @private
+   * @type {object}
+   * @memberof Server
+   */
   private messageListeners: object = {};
 
+  /**
+   * @constructor
+   * Creates an instance of Sidebar.
+   * @memberof Server
+   */
   constructor() {
     console.log('Start Server instance');
+
+    /**
+     * Assign socket and default values
+     */
     this.socket = new Socket({
       server: CONFIG().WEBSOCKET.URL,
       pingPongTime: 10000,
@@ -28,19 +87,48 @@ class Server {
     this.cid = this.getClientId();
   }
 
+  /**
+   * this method send requests to Server
+   * @public
+   * @param {IRequest} req
+   * @returns {Promise<any>}
+   * @memberof Server
+   */
   public request(req: IRequest): Promise<any> {
+
+    /**
+     * Define aaa
+     * @const
+     * @type {object}
+     */
     const aaa = AAA.getInstance();
+
+    /**
+     * Define credential
+     * @const
+     * @type {object}
+     */
     const credential = aaa.getCredentials();
     if (!req._reqid) {
       req._reqid = this.getRequestId();
     }
 
+    /**
+     * add client version and client id to the request object
+     * @const
+     * @type {object}
+     */
     const socketRequest: any = {
       ...req,
       _cver: CONFIG().APP_VERSION,
       _cid: this.cid,
     };
 
+    /**
+     * add Session key and Session ID to the request object
+     * @const
+     * @type {object}
+     */
     if (credential.sk && credential.sk !== 'null') {
       socketRequest._sk = credential.sk;
     }
@@ -49,6 +137,12 @@ class Server {
       socketRequest._ss = credential.ss;
     }
 
+    /**
+     * Create promise functions for reslove and rejecting the requests
+     * @var
+     * @callback
+     * @type {Promise}
+     */
     let internalResolve;
     let internalReject;
 
@@ -61,6 +155,9 @@ class Server {
       }
     });
 
+    /**
+     * Add request to the queue manager
+     */
     this.queue.push({
       state: 0,
       reqId: req._reqid,
@@ -73,6 +170,12 @@ class Server {
     return promise;
   }
 
+  /**
+   * TODO : use this method or delete it
+   * @param {function} callback
+   * @returns {*}
+   * @memberof Server
+   */
   public addMessageListener(callback: (syncObj: any) => void): any {
     const listenerId = 'listener_' + Unique.get();
     this.messageListeners[listenerId] = callback;
@@ -82,40 +185,87 @@ class Server {
     return canceler;
   }
 
+  /**
+   * add callback function to socket state change event
+   * @param {function} callback
+   * @memberof Server
+   * @public
+   */
   public onConnectionStateChange(callback: (state: SocketState) => void) {
     this.socket.onStateChanged = callback;
   }
 
+  /**
+   * call all message listener
+   * @name Server#callMessageListeners
+   * @private
+   * @param {object} message
+   * @memberof Server
+   */
   private callMessageListeners(message: object) {
     Object.keys(this.messageListeners).forEach((key: string) => {
       this.messageListeners[key](message);
     });
   }
 
+  /**
+   * Reject the request when server response takes long time
+   * @private
+   * @memberof Server
+   */
   private handleTimeout = (requestId) => {
     return setTimeout(() => {
 
+      /**
+       * @const errorData
+       * @type {IErrorResponseData}
+       */
       const errorData: IErrorResponseData = {
         err_code: 1000,
         items: [],
       };
 
+      /**
+       * generate respone object when the server do not respone in enogh time
+       */
       const fakeResponse: IResponse = {
         _reqid: requestId,
         status: 'err',
         data: errorData,
       };
 
+      /**
+       * send response to the sender service
+       */
       this.response(JSON.stringify(fakeResponse));
     }, TIMEOUT_TIME);
   }
 
+  /**
+   * generates uniqe ids for request
+   * @private
+   * @returns {string} reqId
+   * @memberof Server
+   * @generator
+   */
   private getRequestId(): string {
     this.reqId++;
     return 'REQ' + this.reqId;
   }
 
+  /**
+   * parse server response to usable object in app
+   * @private
+   * @param {string} res
+   * @returns {object}
+   * @memberof Server
+   */
   private response(res: string): void {
+
+    /**
+     * @const response
+     * @type {object}
+     */
     const response = JSON.parse(res);
 
     // start the queue after receiving hi
@@ -136,6 +286,10 @@ class Server {
       return;
     }
 
+    /**
+     * @const queueItem
+     * @type {object}
+     */
     const queueItem = this.queue[itemIndex];
 
     // resole request
@@ -155,10 +309,22 @@ class Server {
     this.queue.splice(itemIndex, 1);
   }
 
+  /**
+   * generates string from request and send to the api
+   * @private
+   * @param {*} request
+   * @memberof Server
+   */
   private sendRequest(request: any) {
     this.socket.send(JSON.stringify(request));
   }
 
+  /**
+   * start sending all queued requests
+   * @private
+   * @function
+   * @memberof Server
+   */
   private startQueue() {
     this.queue.map((request) => {
       if (request.state === 0) {
@@ -167,6 +333,14 @@ class Server {
     });
   }
 
+  /**
+   * generates a client id
+   * @private
+   * @function
+   * @generator
+   * @memberof Server
+   * @returns {string}
+   */
   private getClientId(): string {
     const deviceName = Client.getDevice();
     const device = deviceName ? 'mobile' : 'desktop';
