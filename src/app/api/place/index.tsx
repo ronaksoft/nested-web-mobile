8095dc9cfeb48d648c6a40197d6503959f78e41c
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2017-07-27 12:21:11 +0430

    Add api/place documents

--- src/app/api/place/index.tsx
+++ src/app/api/place/index.tsx
@@ -1,3 +1,13 @@
+/**
+ * @file api/place/index.tsx
+ * @author Soroush Torkzadeh <sorousht@nested.com>
+ * @desc All APIs are related to Place
+ * @export PlaceApi
+ * Documented by:         Soroush Torkzadeh
+ * Date of documentation: 2017-07-27
+ * Reviewed by:           -
+ * Date of review:        -
+ */
 import Api from './../index';
 import IPlaceSuggestComposeRequest from './interfaces/IPlaceSuggestComposeRequest';
 import IAccountPlacesRequest from './interfaces/IAccountPlacesRequest';
@@ -7,6 +17,12 @@ import IGetRequest from './interfaces/IGetRequest';
 import IPlace from './interfaces/IPlace';
 
 export default class PlaceApi {
+  /**
+   * @desc An instance of Api
+   * @prop api
+   * @private
+   * @memberof PlaceApi
+   */
   private api;
 
   constructor() {
@@ -20,6 +36,13 @@ export default class PlaceApi {
     });
   }
 
+  /**
+   * @func getAllPlaces
+   * @desc Get all places that the authenticated user is a member
+   * @param {IAccountPlacesRequest} getAllPlacesRequest 
+   * @returns {Promise<any>} 
+   * @memberof PlaceApi
+   */
   public getAllPlaces(getAllPlacesRequest: IAccountPlacesRequest): Promise<any> {
     return this.api.server.request({
       cmd: 'account/get_all_places',
@@ -30,6 +53,14 @@ export default class PlaceApi {
     });
   }
 
+  /**
+   * @func getUnreads
+   * @desc Retrieves the place unread posts count. If you want to get the number of
+   * unread posts in the place and all children, set `subs` to true
+   * @param {IGetUnreadsRequest} IGetUnreadsRequest 
+   * @returns {Promise<any>} 
+   * @memberof PlaceApi
+   */
   public getUnreads(IGetUnreadsRequest: IGetUnreadsRequest): Promise<any> {
     return this.api.server.request({
       cmd: 'place/count_unread_posts',
@@ -40,6 +71,13 @@ export default class PlaceApi {
     });
   }
 
+  /**
+   * @func get
+   * @desc Requests for a place data. This API provides everything you need to know about the place
+   * @param {IGetRequest} data 
+   * @returns {Promise<any>} 
+   * @memberof PlaceApi
+   */
   public get(data: IGetRequest): Promise<any> {
     return this.api.request({
       cmd: 'place/get',
@@ -47,6 +85,12 @@ export default class PlaceApi {
     });
   }
 
+  /**
+   * @func getInvitations
+   * @desc Retrieves a list of the given place pending invitations
+   * @returns {Promise<any>} 
+   * @memberof PlaceApi
+   */
   public getInvitations(): Promise<any> {
     return this.api.request({
       cmd: 'account/get_invitations',
