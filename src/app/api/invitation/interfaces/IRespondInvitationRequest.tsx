2a708fd3931798b038e3e00ff54051123d19f42e
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2017-07-27 12:20:55 +0430

    Add invitation documents

--- src/app/api/invitation/interfaces/IRespondInvitationRequest.tsx
+++ src/app/api/invitation/interfaces/IRespondInvitationRequest.tsx
@@ -1,3 +1,15 @@
+/**
+ * @file api/invitation/interfaces/IRespondInvitationRequest.tsx
+ * @author Soroush Torkzadeh <sorousht@nested.com>
+ * @desc Interface of responding an invitation
+ * @export IRespondInvitationRequest
+ * @exports {RespondType}
+ * Documented by:         Soroush Torkzadeh
+ * Date of documentation: 2017-07-27
+ * Reviewed by:           -
+ * Date of review:        -
+ */
+
 import RespondType from './RespondType';
 
 interface IRespondInvitationRequest {
--- src/app/api/invitation/interfaces/RespondType.tsx
+++ src/app/api/invitation/interfaces/RespondType.tsx
@@ -1,3 +1,13 @@
+/**
+ * @file api/invitation/interfaces/RespondType.tsx
+ * @author Soroush Torkzadeh <sorousht@nested.com>
+ * @desc When somebody invites you to a place, you have two choices.
+ * @export RespondType
+ * Documented by:         Soroush Torkzadeh
+ * Date of documentation: 2017-07-27
+ * Reviewed by:           -
+ * Date of review:        -
+ */
 type RespondType = 'accept' | 'ignore';
 
 export default RespondType;
