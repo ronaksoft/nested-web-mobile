4ab36690d183812c4ea5b364b5b3d3d5bdc138f0
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2017-07-29 18:28:30 +0430

    Add api/index documents

--- src/app/api/index.tsx
+++ src/app/api/index.tsx
@@ -1,7 +1,21 @@
+/**
+ * @file api/index.tsx
+ * @author Soroush Torkzadeh <sorousht@nested.com>
+ * @desc All APIs are related to Post
+ * @export PlaceApi
+ * Documented by:         Soroush Torkzadeh
+ * Date of documentation: 2017-07-29
+ * Reviewed by:           -
+ * Date of review:        -
+ */
 import {Server, IRequest} from 'services/server';
 import {IResponse} from 'services/server';
 import Unique from './../services/utils/unique';
 
+/**
+ * @class Api
+ * @desc Base of all Api classes
+ */
 class Api {
   private hasCredential: boolean = false;
   private static instance;
@@ -14,6 +28,13 @@ class Api {
     // start api service
   }
 
+  /**
+   * @func getInstance
+   * @desc Creates an instance of Api and keeps it singletonewhile the app is running
+   * @static
+   * @returns {Api} 
+   * @memberof Api
+   */
   public static getInstance(): Api {
     if (!Api.instance) {
       Api.instance = new Api();
@@ -21,14 +42,33 @@ class Api {
     return Api.instance;
   }
 
+  /**
+   * @func setHasCredential
+   * @desc Sets value of hasCredential
+   * @param {boolean} value 
+   * @memberof Api
+   */
   public setHasCredential(value: boolean) {
     this.hasCredential = value;
   }
 
+  /**
+   * @func getHasCredential
+   * @desc Returns value of hasCredential
+   * @returns 
+   * @memberof Api
+   */
   public getHasCredential() {
     return this.hasCredential;
   }
 
+  /**
+   * @func request
+   * @desc Sends a request and resolves/rejects based on the response
+   * @param {IRequest} req
+   * @returns {Promise<any>}
+   * @memberof Api
+   */
   public request(req: IRequest): Promise<any> {
     return new Promise((resolve, reject) => {
       this.getServer().request(req).then((response: IResponse) => {
@@ -40,7 +80,7 @@ class Api {
       }).catch(reject);
     });
   }
-
+  // TODO: Ask sina to explain these functions
   public addSyncActivityListener(callback: (syncObj: any) => void): any {
     const listenerId = 'listener_' + Unique.get();
     console.log(this.syncActivityListeners, listenerId);
