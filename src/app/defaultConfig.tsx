8e8d1d9ecbbb57b08cad2f7b77b423e54fe780d4
Sina <e.hosseini.ir@gmail.com>
2017-08-03 16:47:18 +0430

    read config from env

--- src/app/defaultConfig.tsx
+++ src/app/defaultConfig.tsx
@@ -1,23 +1,23 @@
 export default {
-    APP_CLIENT_ID: 'WEBAPP_DEVELOPMENT',
-    APP_VERSION: 353,
-    DOMAIN: '_DOMAIN_',
-    SIGN_OUT_TARGET: 'https://nested.me',
-    WEBSOCKET: {
-        URL: 'ws://cyrus.ronaksoftware.com:81',
-        TIMEOUT: 60000,
-        REQUEST_MAX_RETRY_TIMES: 16,
+  APP_CLIENT_ID: 'WEBAPP_DEVELOPMENT',
+  APP_VERSION: 353,
+  DOMAIN: '_DOMAIN_',
+  SIGN_OUT_TARGET: 'https://nested.me',
+  WEBSOCKET: {
+    URL: process.env.NST_WS_CYRUS_URL || 'wss://cyrus.nested.me:443',
+    TIMEOUT: 60000,
+    REQUEST_MAX_RETRY_TIMES: 16,
+  },
+  STORE: {
+    URL: process.env.NST_XERXES_URL || 'https://xerxes.nested.me',
+    TOKEN_EXPMS: 3550000,
+  },
+  REGISTER: {
+    AJAX: {
+      URL: process.env.NST_HTTP_CYRUS_URL || 'https://cyrus.nested.me:444',
     },
-    STORE: {
-        URL: 'http://xerxes.ronaksoftware.com:83',
-        TOKEN_EXPMS: 3550000,
-    },
-    REGISTER: {
-        AJAX: {
-            URL: 'http://cyrus.ronaksoftware.com:82',
-        },
-    },
-    GRAND_PLACE_REGEX: /^[a-zA-Z][a-zA-Z0-9-]{3,30}[a-zA-Z0-9]$/,
-    GOOGLE_ANALYTICS_TOKEN: 'UA-92612481-1',
-    UPLOAD_SIZE_LIMIT: 209715200,
+  },
+  GRAND_PLACE_REGEX: /^[a-zA-Z][a-zA-Z0-9-]{3,30}[a-zA-Z0-9]$/,
+  GOOGLE_ANALYTICS_TOKEN: 'UA-92612481-1',
+  UPLOAD_SIZE_LIMIT: 209715200,
 };
