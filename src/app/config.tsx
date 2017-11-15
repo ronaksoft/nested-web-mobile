6adcd9b9ac8db6ba455645d75422bbd58bce4c8b
Hamidreza Kazemi <hamidrezakks@gmail.com>
2017-11-15 18:26:35 +0330

    update

--- src/app/config.tsx
+++ src/app/config.tsx
@@ -2,19 +2,37 @@ function generateConfig(): any {
   const windowObj: Window = window;
   if (!window.hasOwnProperty('__NESTED_CONFIG__')) {
     window.__NESTED_CONFIG__ = {
+      APP_CLIENT_ID: 'WEBAPP_DEVELOPMENT',
+      APP_VERSION: 353,
       DOMAIN: 'nested.me',
+      SIGN_OUT_TARGET: 'https://nested.me',
       WEBSOCKET: {
-        URL: 'wss://cyrus.nested.me:443',
+        URL: '_WS_CYRUS_CYRUS_URL_CONF_',
+        TIMEOUT: 60000,
+        REQUEST_MAX_RETRY_TIMES: 16,
       },
       STORE: {
-        URL: 'https://xerxes.nested.me:80',
+        URL: '_XERXES_URL_CONF_',
+        TOKEN_EXPMS: 3550000,
       },
       REGISTER: {
         AJAX: {
-          URL: 'wss://cyrus.nested.me:443',
+          URL: '_HTTP_CYRUS_URL_CONF_',
         },
       },
+      GRAND_PLACE_REGEX: /^[a-zA-Z][a-zA-Z0-9-]{3,30}[a-zA-Z0-9]$/,
+      GOOGLE_ANALYTICS_TOKEN: 'UA-92612481-1',
+      UPLOAD_SIZE_LIMIT: 209715200,
     };
+  }
+  if (windowObj.__NESTED_CONFIG__.WEBSOCKET.URL.indexOf('://') === -1) {
+    windowObj.__NESTED_CONFIG__.WEBSOCKET.URL = 'wss://webapp.ronaksoftware.com:81';
+  }
+  if (windowObj.__NESTED_CONFIG__.STORE.URL.indexOf('://') === -1) {
+    windowObj.__NESTED_CONFIG__.STORE.URL = 'https://webapp.ronaksoftware.com:83';
+  }
+  if (windowObj.__NESTED_CONFIG__.REGISTER.AJAX.URL.indexOf('://') === -1) {
+    windowObj.__NESTED_CONFIG__.REGISTER.AJAX.URL = 'https://webapp.ronaksoftware.com:81';
   }
   return {
     APP_CLIENT_ID: 'WEBAPP_DEVELOPMENT',
