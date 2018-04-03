2a9916706d8ee9fafd8ea74ea760fb1140cc08ba
Hamidreza KK <hamidrezakks@gmail.com>
2018-04-03 16:02:15 +0430

    member section view completed

--- src/app/scenes/private/place/members/index.tsx
+++ src/app/scenes/private/place/members/index.tsx
@@ -12,6 +12,14 @@ import PlaceApi from '../../../../api/place';
 import IGetWithSkipRequest from '../../../../api/place/interfaces/IGetWithSkipRequest';
 import IUser from '../../../../api/account/interfaces/IUser';
 import {IcoN, UserAvatar, FullName, Loading} from 'components';
+import * as _ from 'lodash';
+
+const style = require('./members.css');
+
+interface IMUser extends IUser {
+  tmpManger: boolean;
+  tmpEditing: boolean;
+}
 
 /**
  * @interface IProps
@@ -47,7 +55,9 @@ interface IState {
 
   placeId: string;
 
-  members: IUser[];
+  members: IMUser[];
+
+  editMode: boolean;
 }
 
 /**
@@ -58,6 +68,7 @@ interface IState {
 class Members extends React.Component<IProps, IState> {
 
   private placeApi: PlaceApi;
+
   /**
    * Creates an instance of Members.
    * @constructor
@@ -77,6 +88,7 @@ class Members extends React.Component<IProps, IState> {
       reachedTheEnd: false,
       placeId: props.params.placeId,
       members: [],
+      editMode: false,
     };
   }
 
@@ -111,20 +123,47 @@ class Members extends React.Component<IProps, IState> {
     };
 
     this.placeApi.getMangers(params).then((users) => {
-      this.addToMembers(users);
+      this.addToMembers(users, true);
     });
 
     this.placeApi.getMembers(params).then((users) => {
-      this.addToMembers(users);
+      this.addToMembers(users, false);
     });
-    console.log('componentDidMount');
   }
 
-  private addToMembers(users: IUser[]) {
+  private addToMembers(users: IUser[], manager: boolean) {
     const temp = this.state.members;
-    temp.push.apply(temp, users);
+    users.forEach((user) => {
+      const tmp = user as IMUser;
+      tmp.tmpEditing = false;
+      tmp.tmpManger = manager;
+      temp.push(tmp);
+    });
     this.setState({
       members: temp,
+    });
+  }
+
+  private toggleMoreOpts = (id: string) => {
+    const index = _.findIndex(this.state.members, {_id: id});
+    const tempList = this.state.members;
+    if (index > -1) {
+      tempList[index].tmpEditing = !tempList[index].tmpEditing;
+    }
+    this.setState({
+      members: tempList,
+      editMode: tempList[index].tmpEditing,
+    });
+  }
+
+  private closeAll = () => {
+    const tempList = this.state.members;
+    tempList.map((member) => {
+      member.tmpEditing = false;
+    });
+    this.setState({
+      members: tempList,
+      editMode: false,
     });
   }
 
@@ -137,11 +176,62 @@ class Members extends React.Component<IProps, IState> {
   public render() {
     return (
       <div>
+        {this.state.editMode &&
+        <div className={style.editModeOverlay} onClick={this.closeAll}/>
+        }
         {this.state.members.map((member) => (
-          <div key={'member_' + member._id}>
-            <UserAvatar user_id={member} size={32} borderRadius={'16px'}/>
-            <FullName user_id={member}/>
-            <span>{member._id}</span>
+          <div key={'member_wrapper_' + member._id}>
+            <div key={'member_' + member._id} className={style.item + ' ' + (member.tmpEditing ? style.editMode : '')}>
+              <div className={style.userAvatar}>
+                <UserAvatar user_id={member} size={32} borderRadius={'16px'}/>
+                {member.tmpManger && (
+                <div className={style.managerBadge}>
+                  <IcoN size={16} name="crown16"/>
+                </div>
+                )}
+              </div>
+              <div className={style.user}>
+              <span className={style.userName}>
+                <FullName user_id={member}/>
+              </span>
+                <span className={style.userId}>{member._id}</span>
+              </div>
+              <div className={style.moreOption}>
+                <a onClick={this.toggleMoreOpts.bind(this, member._id)}>
+                  {!member.tmpEditing &&
+                  <IcoN size={24} name="more24"/>
+                  }
+                  {member.tmpEditing &&
+                  <IcoN size={24} name="xcross24"/>
+                  }
+                </a>
+              </div>
+            </div>
+            {member.tmpEditing && (
+              <div>
+                <div className={style.item + ' ' + style.secondary}>
+                  <div className={style.icon}>
+                    {member.tmpManger &&
+                    <IcoN size={16} name="person16"/>
+                    }
+                    {!member.tmpManger &&
+                    <IcoN size={16} name="crown16"/>
+                    }
+                  </div>
+                  <div className={style.label}>
+                    {member.tmpManger ? 'Demote' : 'Promote'}
+                  </div>
+                </div>
+                <div className={style.item + ' ' + style.secondary + ' ' + style.red}>
+                  <div className={style.icon}>
+                    <IcoN size={16} name="exit16"/>
+                  </div>
+                  <div className={style.label}>
+                    Remove from Place
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         ))}
       </div>
